9c9ed571d86fdb815ce11ec80a64b841264adf1f
Yusheng <tamamali@icloud.com>
2015-08-06 00:48:30 +0800

    update actions

--- js/result.js
+++ js/result.js
@@ -5,7 +5,7 @@ function shuffle(o){
 $(document).ready(function() {
   name = Cookies.get('name');
   var people = new Array("Jeffy Sac", "球球", "崑元", "腿骨");
-  var action = new Array("一起夜衝唱K", "一起騎腳踏車上阿里山", "一起美照三連拍", "對唱林俊傑的小酒窩", "一起芭雷舞三連拍", "肚子磨肚子", "互相地趴", "互相壁咚", "公主抱", "臉貼臉五秒鐘", "一起重現開源魔法少女");
+  var action = new Array("一起美照三連拍", "對唱林俊傑的小酒窩", "一起芭雷舞三連拍", "肚子磨肚子", "互相地趴", "互相壁咚", "公主抱", "臉貼臉五秒鐘", "一起重現開源魔法少女", "一起表演濕滑變臉", "跟台大開源的兩屆社長一起拍女神三連拍", "找十個人一起做村長的招牌動作", "跟活動組長一起可愛三連拍");
 
   people = shuffle(people);
   action = shuffle(action);
