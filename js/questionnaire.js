eda9c77f699cfd98bf9e6b3d25221dc88e160b2c
Yusheng <tamamali@icloud.com>
2015-08-06 00:49:14 +0800

    fixed bugs: putting author name in the gameplayer name as default Q_Q

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,8 +1,7 @@
 $("#questionnaire").submit(function( event ) {
-  alert( "Handler for .submit() called." );
   event.preventDefault();
 
-  name = $('[name=name]').val();
+  name = $('[name="name"]').val();
   console.log(name);
 
   teammates = document.getElementsByName('teammates');
@@ -11,11 +10,11 @@ $("#questionnaire").submit(function( event ) {
   player = [teammates[0].value, teammates[1].value, teammates[2].value];
   console.log(player);
 
-  Cookies.set('name', 'Yusheng Li');
+  Cookies.set('name', name);
 
   Cookies.set('teammates1', player[0]);
   Cookies.set('teammates2', player[1]);
   Cookies.set('teammates3', player[2]);
 
-  location.href='/result.html';
+  location.href='result.html';
 });
