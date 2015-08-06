372a2b392f1a9ad14e8312f4b72159500e3984a8
Yusheng <tamamali@icloud.com>
2015-08-06 12:21:46 +0800

    fixed bugs: arrays not correctly shuffled

--- js/result.js
+++ js/result.js
@@ -9,9 +9,7 @@ $(document).ready(function() {
   var people = new Array();
   var action = new Array("一起美照三連拍", "對唱林俊傑的小酒窩", "對唱「找不到」", "一起芭蕾舞三連拍", "肚子磨肚子", "互相地趴", "互相壁咚", "公主抱", "臉貼臉五秒鐘", "一起重現開源魔法少女", "一起表演濕滑變臉", "跟台大開源的兩屆社長一起拍女神三連拍", "找十個人一起做村長的招牌動作", "跟活動組長一起可愛三連拍");
 
-  people = shuffle(people);
-  action = shuffle(action);
-  staff  = shuffle(staff);
+
 
   if(name == "undefined" || name == null || name == "" ) {
     $('span#gameplayer1').html(staff[0]);
@@ -36,6 +34,10 @@ $(document).ready(function() {
   } else {
     people.push(Cookies.get('teammates3'));
   }
+
+  people = shuffle(people);
+  action = shuffle(action);
+  staff  = shuffle(staff);
 
   $('span#gameplayer2').html(people[0]);
   $('span#gameaction').html(action[0]);
